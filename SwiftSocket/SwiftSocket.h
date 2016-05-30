276218978c01e55189c1a81c176f77769ec33abd
Andrew James Cavanagh <acavanagh@gmail.com>
2016-05-30 12:13:34 -0500

    Initial Commit

---
+++ SwiftSocket/SwiftSocket.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSocket.h
+//  SwiftSocket
+//
+//  Created by Andrew Cavanagh on 5/30/16.
+//  Copyright © 2016 Andrew Cavanagh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSocket.
+FOUNDATION_EXPORT double SwiftSocketVersionNumber;
+
+//! Project version string for SwiftSocket.
+FOUNDATION_EXPORT const unsigned char SwiftSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSocket/PublicHeader.h>
+
+
